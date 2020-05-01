b31d134d78f90b4dc5745f0825c70a9292558666
Kurt Moser <kurt@suvaline.com>
2020-05-01 22:12:04 +0300

    Use stack to solve day 5

--- aoc2018/day05/polymer.py
+++ aoc2018/day05/polymer.py
@@ -1,30 +1,14 @@
-from string import ascii_lowercase
-
 class Polymer():
     def __init__(self, polymer):
         self.polymer = polymer
 
     def reduce(self):
-        reducable_pairs = self._build_reducable_pairs()
-        polymer = self.polymer
-        polymer_length = len(polymer)
+        stack = []
 
-        while True:
-            for pair in reducable_pairs:
-                polymer = polymer.replace(pair, '')
+        for unit in self.polymer:
+            if stack and stack[-1] == unit.swapcase():
+                stack.pop()
+            else:
+                stack.append(unit)
 
-            if len(polymer) == polymer_length:
-                break
-
-            polymer_length = len(polymer)
-
-        return polymer
-
-    def _build_reducable_pairs(self):
-        pairs = []
-
-        for char in ascii_lowercase:
-            pairs.append(char + char.upper())
-            pairs.append(char.upper() + char)
-
-        return pairs
+        return ''.join(stack)
